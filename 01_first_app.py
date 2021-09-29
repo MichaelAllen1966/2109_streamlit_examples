c105b0ab483d3b3165e462e2aab56965d6d3dd31
Michael Allen <michael.allen1966@gmail.com>
2021-09-29 13:46:17 +0100

    Update 01_frst app

--- 01_first_app.py
+++ 01_first_app.py
@@ -13,3 +13,25 @@ df['Height'] = [1.75, 1.85, 1.70]
 # Use st.write to display 'stuff'
 st.write("Hello world! Here is a Pandas DataFrame.....")
 st.write(df)
+
+# Produce a chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Produce a map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+# Add a checkbox
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
